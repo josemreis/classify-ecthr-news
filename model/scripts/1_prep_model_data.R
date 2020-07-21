1255d00b18db5e7388848ed5ae9b50f3dd26943a
josemreis <josembcsreis@gmail.com>
2020-07-22 00:42:19 +0100

    Filled missing dates in the model data

--- model/scripts/1_prep_model_data.R
+++ model/scripts/1_prep_model_data.R
@@ -18,16 +18,30 @@ stringdist <- map_df(sd_list, read_csv) %>%
 
 #### Date distance, article nchr and source lang ------------------------------------------------------------------------
 ### load the articles data
-arts_all <- read_csv(articles_data_path) %>%
-  mutate(article_nchar = nchar(text), ## fill in missing date published
-         date_published = replace(date_published, which(is.na(date_published)), ymd(str_extract(article_id, "(?<=\\_)[0-9]{4}\\-[0-9]{1,2}\\-[0-9]{1,2}(?=\\_)"))),
+arts_all <- read_csv(articles_data_path)
+
+## fill in missing dates for denmark and belgium
+to_fill <- read_csv('/home/jmr/Dropbox/Current projects/thesis_papers/transparency, media, and compliance with HR Rulings/ecthr_media&compliance/data/media_data/1_data_collection/alternative_sources/scrape/country-level/Belgium/data/2_ecthrMatch&counts_added.csv.gz') %>%
+  select(article_id, date_published2 = published_date) %>%
+  rbind(
+    read_csv('/home/jmr/Dropbox/Current projects/thesis_papers/transparency, media, and compliance with HR Rulings/ecthr_media&compliance/data/media_data/1_data_collection/alternative_sources/scrape/country-level/Denmark/data/2_ecthrMatch&counts_added.csv.gz') %>%
+      select(article_id, date_published2 = published_date)
+  ) %>% 
+  mutate(date_published2 = as_date(ymd_hms(date_published2)))
+
+## prep the arts data
+arts_data <- left_join(arts_all, to_fill) %>% ## fill in missing
+  mutate(date_published = if_else(is.na(date_published) & !is.na(date_published2),
+                                  date_published2,
+                                  date_published), ## generate the other features
+         date_published = ymd(date_published)) %>%
+  mutate(article_nchar = nchar(text),
          date_distance = as.integer(judgment_date - date_published)) %>%
-  select(ecthr_label, article_id, case_id, text, article_nchar, source_lang_alpha2, date_distance, date_published, judgment_date) # filter the relevant vars
+  select(ecthr_label, article_id, case_id, text, article_nchar, source_lang_alpha2, date_distance, date_published, judgment_date)
 
 #### Make the final dataset ----------------------------------------------------------------------
 ### combine
-dataset_all <- tidy_dtm %>%
-  left_join(arts_all) %>%
+dataset_all <- arts_data %>%
   left_join(stringdist)
 
 ### modeling data
